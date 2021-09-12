e52f15acbed57f9aa36a65a0292b6332fb15d681
Luca <luca.mic92@gmail.com>
2021-09-12 22:30:09 +0200

    laravel comics

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $comics = config('comics');
+    $data = [
+        'series' => $comics
+    ];
+    return view('home', $data);
+})->name('homepage');
+
+Route::get('/comic/{id}', function($id) {
+    $comics = config('comics');
+    if(array_key_exists($id, $comics)) {
+        $fumetto = $comics[$id];
+        $data = [
+            'comic' => $fumetto
+        ];
+        return view('single', $data);
+    }
+    abort(404);
+})->name('comic-details');
+
+Route::get('/contact-us', function() {
+    return view('contact');
+});
